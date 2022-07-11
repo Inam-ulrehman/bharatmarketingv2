af1a81453eb60f1a31a41341474e4c85fec8ae47
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-11 11:36:41 -0400

    form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Contact = () => {
-  return <div>Contact</div>
+  // handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault(e)
+    console.log(e.target)
+  }
+  return (
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3 className='title'> Bharat Marketing </h3>
+        <div className='title-underline'></div>
+        <div>
+          {/* name input */}
+          <label className='form-label' htmlFor='name'>
+            Name
+          </label>
+          <input type='text' className='form-input' />
+          {/* email input */}
+          <label className='form-label' htmlFor='name'>
+            Email
+            <input type='email' className='form-input' />
+          </label>
+          {/* Mobile input */}
+          <label className='form-label' htmlFor='name'>
+            Mobile
+          </label>
+          <input type='text' className='form-input' />
+          {/* textArea input */}
+          <label className='form-label' htmlFor='name'>
+            Inquiry
+          </label>
+          <textarea
+            className='form-textarea'
+            name='Text1'
+            cols='60'
+            rows='5'
+          ></textarea>
+        </div>
+        <button className='btn btn-block' type='submit'>
+          Submit your request
+        </button>
+      </form>
+    </Wrapper>
+  )
 }
-
+const Wrapper = styled.section`
+  @media (max-width: 768px) {
+    margin-top: 3rem;
+  }
+`
 export default Contact
