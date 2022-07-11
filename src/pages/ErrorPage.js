ded6c0b7c5f16585e858e651c1bf3097e31886cb
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-11 14:47:05 -0400

    error

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Footer from '../components/Footer'
+import error from '../images/pageNotFound.svg'
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>
+  return (
+    <Wrapper>
+      <div className='img'>
+        <img src={error} alt='' />
+      </div>
+      <div className='body'></div>
+      <h3>Page not found</h3>
+      <Link className='btn' to='/home'>
+        Back home
+      </Link>
+      <Footer />
+    </Wrapper>
+  )
 }
 
+const Wrapper = styled.section`
+  text-align: center;
+  .img {
+    img {
+      max-width: 400px;
+      max-height: 400px;
+    }
+  }
+`
 export default ErrorPage
