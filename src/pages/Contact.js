f9721985b0368287304194754460c58d81408c74
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-12 11:32:39 -0400

    main

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
 import styled from 'styled-components'
 import Footer from '../components/Footer'
 
+const initialState = {
+  name: '',
+  email: '',
+  mobile: '',
+  inquiry: '',
+}
 const Contact = () => {
+  const [state, setState] = useState(initialState)
   // handleSubmit
   const handleSubmit = (e) => {
+    const { name, email, mobile, inquiry } = state
     e.preventDefault(e)
-    toast.success('Soon a member of our team will be in touch with you.')
+    console.log(e.target.name)
+    if (!name || !email || !mobile || !inquiry) {
+      return toast.warning('Please fill in all the fields ')
+    } else {
+      return toast.success('Request successfully submitted.')
+    }
+  }
+
+  // handleChange
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setState({ ...state, [name]: value })
   }
   return (
     <Wrapper>
@@ -19,26 +40,50 @@ const Contact = () => {
           <label className='form-label' htmlFor='name'>
             Name
           </label>
-          <input type='text' className='form-input' />
+          <input
+            type='text'
+            className='form-input'
+            name='name'
+            id='name'
+            value={state.name}
+            onChange={handleChange}
+          />
           {/* email input */}
-          <label className='form-label' htmlFor='name'>
+          <label className='form-label' htmlFor='email'>
             Email
-            <input type='email' className='form-input' />
+            <input
+              type='email'
+              className='form-input'
+              name='email'
+              id='email'
+              value={state.email}
+              onChange={handleChange}
+            />
           </label>
           {/* Mobile input */}
-          <label className='form-label' htmlFor='name'>
+          <label className='form-label' htmlFor='mobile'>
             Mobile
           </label>
-          <input type='text' className='form-input' />
+          <input
+            type='text'
+            className='form-input'
+            name='mobile'
+            id='mobile'
+            value={state.mobile}
+            onChange={handleChange}
+          />
           {/* textArea input */}
-          <label className='form-label' htmlFor='name'>
+          <label className='form-label' htmlFor='inquiry'>
             Inquiry
           </label>
           <textarea
             className='form-textarea'
-            name='textarea'
             cols='60'
             rows='5'
+            name='inquiry'
+            id='inquiry'
+            value={state.inquiry}
+            onChange={handleChange}
           ></textarea>
         </div>
         <button className='btn btn-block' type='submit'>
